36ad99f737af406d1f74a834d96e0ec42102d121
Riku Brandt <brandtriku@gmail.com>
2025-01-05 17:23:53 +0200

    Ex 1.07

--- part1/log_output/log_output.py
+++ part1/log_output/log_output.py
@@ -1,15 +1,26 @@
-import datetime
 import time
 import random
+from datetime import datetime
 import string
+from flask import Flask, jsonify
 
-def gen_random_and_output():
-    random_string = ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
+app = Flask(__name__)
 
-    while True:
-        current_datetime = datetime.datetime.now()
+random_string = ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
+current_timestamp = datetime.now()
 
-        print(str(current_datetime) + " "+ random_string, flush=True)
-        time.sleep(5)
+@app.route("/")
+def home():
+    return "Log Output APP."
 
-gen_random_and_output()
+@app.route("/status")
+def status():
+    return jsonify({
+        "timestamp": current_timestamp,
+        "random_string": random_string
+    })
+
+if __name__ == "__main__":
+    print(f"Server started with random string: {random_string}")
+    app.run(host="0.0.0.0", port=5000)
+
