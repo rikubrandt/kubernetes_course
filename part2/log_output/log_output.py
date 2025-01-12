301166f28ae1c0f7ee1d741d4f1e1410deca6e69
Riku Brandt <brandtriku@gmail.com>
2025-01-12 15:51:27 +0200

    2.07

--- part2/log_output/log_output.py
+++ part2/log_output/log_output.py
@@ -1,23 +1,25 @@
 import os
 import hashlib
 import requests
-from flask import Flask
+from flask import Flask, jsonify
 from datetime import datetime
 
 app = Flask(__name__)
 
 PINGPONG_URL = os.getenv("PINGPONG_URL", "http://ping-pong-svc:5000/count")
-message_env = os.getenv("MESSAGE")
+MESSAGE_ENV = os.getenv("MESSAGE")
 
 @app.route("/")
 def home():
-    """Fetch the pong count from Ping-Pong Application and display the log output."""
+    """Fetch and display the pong count from Ping-Pong Application."""
     try:
         with open("/config/information.txt", "r") as file:
             file_content = file.read().strip()
     except FileNotFoundError:
         file_content = "No file content found"
+
     try:
+        # Fetch count from ping-pong application
         response = requests.get(PINGPONG_URL, timeout=2)
         response.raise_for_status()
         count = response.json().get("count", 0)
@@ -28,7 +30,9 @@ def home():
     timestamp = datetime.utcnow().isoformat()
     hashed_timestamp = hashlib.sha256(timestamp.encode()).hexdigest()
 
-    return f"File: {file_content} \n Env var: {message_env} \n{timestamp}: {hashed_timestamp}. Ping / Pongs: {count}"
+    return (f"File: {file_content} \n "
+            f"Env var: {MESSAGE_ENV} \n "
+            f"{timestamp}: {hashed_timestamp}. Ping / Pongs: {count}")
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
